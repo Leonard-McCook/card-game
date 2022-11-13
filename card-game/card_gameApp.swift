9d1930e1cd2bd4f6a26d5d27ff7c5d01687d3b46
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-11-13 14:05:49 -0500

    Initial Commit

---
+++ card-game/card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  card_gameApp.swift
+//  card-game
+//
+//  Created by Leonard McCook-Carr on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
